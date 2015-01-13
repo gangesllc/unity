7ec71db887c1e6a91aa787bb65f614772c894a54
nalavenkata <siva@nalavenkata.com>
2015-01-12 18:51:25 -0800

    runner

---
+++ runner/Assets/scripts/generateTerrain.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class generateTerrain : MonoBehaviour {
+
+	/***************************************************************************
+	 * Assign this script to the runner. This will generate the terrain dynamically
+	 * The terrain generation will always be with respect to the player's position 
+	 * Step 1 : Raycast in the negative Y direction ( Assuming normaly gravity )
+	 * 			// This should give the current terrain block on which the character is standing
+	 * Step 2 : 
+	 */
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
