c1d96871e82de4dc531fea157a497cf36b64e0f1
nalavenkata <siva@nalavenkata.com>
2015-01-18 09:22:11 -0800

    runner

--- runner/Assets/scripts/generateTerrain.cs
+++ runner/Assets/scripts/generateTerrain.cs
@@ -11,13 +11,55 @@ public class generateTerrain : MonoBehaviour {
 	 * Step 2 : 
 	 */
 
+
+	// The list of prefabs that will be used to construct the terrain
+	public GameObject[] prefabs;
+
+	// The speed at which the terrain runs
+	public float speed;
+
+
+	private GameObject[] path;
+
+	// This is the center piece of the terrain objects based on which the remaining path is built.
+	// This is reset to the current terrain object after the terrain piece is traversed. 
+	// The traversed traversed is recycled back to the object pool
+	// All the terrain objects 
+	private terrainObject rootTerrainObject;
 	// Use this for initialization
+
 	void Start () {
-		
+		path = new GameObject[10];
+		terrainObject to = new terrainObject();
+
+		//path[0] = new GameObject();
+		path[0] = Instantiate (prefabs[0],new Vector3(0,0,0), Quaternion.identity) as GameObject;
+		//path[1] = Instantiate (prefabs[1],new Vector3(0,0,6), Quaternion.identity) as GameObject;
+
+		foreach ( Transform t in path[0].transform )
+		{
+			//print ("name is " + t.name);
+		}
+
+		Transform[] ts = path[0].GetComponentsInChildren  <Transform> ();
+		foreach ( Transform t in ts )
+		{
+			print ( t.name );
+		}
+
+		//Transform t = path[0].transform.Find ("front");
+
+		//path[0].transform.parent = gameObject.transform;
+		//path[1].transform.parent = gameObject.transform;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		for ( int i = 0 ; i < 2 ; i++ )
+		{
+			path[i].transform.Translate (Vector3.back * Time.deltaTime * speed);
+		}
 	}
 }
---
+++ runner/Assets/scripts/terrainObject.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class terrainObject : MonoBehaviour {
+
+
+	public terrainObject self;
+	public terrainObject front;
+	public terrainObject back;
+	public terrainObject left;
+	public terrainObject right;
+
+	public void setSelf(GameObject go ) 
+	{
+		self = go ;
+	}
+
+	/* Gets the front game object associated with the current game object
+	 * 
+	 */	
+	public getFront()
+	{
+		
+	}
+	
+	/* Gets the back game object associated with the current game object
+	 * 
+	 */
+	public getBack()
+	{
+	}
+	
+	/* Gets the right game object associated with the current game object
+	 * 
+	 */
+	public getRight()
+	{
+	}
+	
+	/* Gets the left game object 
+	 * 
+	 */
+	public getLeft()
+	{
+	}
+
+}
